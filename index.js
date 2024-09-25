74afe43f47b9d4adb20aa943de9507a19d573371
CaiozinhoTenebroso <caiovictornascimento2004@gmail.com>
2024-09-25 12:57:55 -0300

    Resultado final paizao ta on

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 var n1 = document.getElementById("btn1");
 var n2 = document.getElementById("btn2");
 
-function add(n1,n2){
-    return n1 + n2
+function add(n1, n2) {
+    return n1 + n2;
 }
 
-function subtration(n1,n2){
-    return n1 - n2
+function subtraction(n1, n2) {
+    return n1 - n2;
 }
 
-function multiply(n1,n2){
-    return n1 * n2
+function multiply(n1, n2) {
+    return n1 * n2;
 }
 
-function division(n1,n2){
-    
+function division(n1, n2) {
+    if (n2 === 0) {
+        return "Cannot divide by zero!";
+    }
+    return n1 / n2;
 }
 
-
-function operate(n1,n2,op){
-
+function operate(n1, n2, op) {
+    switch (op) {
+        case "add":
+            return add(n1, n2);
+        case "subtraction":
+            return subtraction(n1, n2);
+        case "multiply":
+            return multiply(n1, n2);
+        case "division":
+            return division(n1, n2);
+        default:
+            return null;
+    }
 }
 
 document.getElementById("btnsupreme").addEventListener("click", function() {
     var number1 = parseFloat(n1.value);
     var number2 = parseFloat(n2.value);
-    var result = number1 + number2; //
-    alert("Resultado: " + result);
+    var operation = document.getElementById("operation").value;
+    var result = operate(number1, number2, operation);
+    
+    // Mostrar o resultado na div
+    document.getElementById("result").innerText = "Resultado: " + result;
 });
